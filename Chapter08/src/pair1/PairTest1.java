b2ccf1e2dd7d260f73a621828290d9c8a53e2696
MS-Stone <wenhao003@e.ntu.edu.sg>
2020-03-02 19:10:20 +0800

    "for each" has better performance than the "for".

--- Chapter08/src/pair1/PairTest1.java
+++ Chapter08/src/pair1/PairTest1.java
@@ -24,9 +24,9 @@ class ArrayAlg {
         if(a == null || a.length == 0) return null;
         String min = a[0];
         String max = a[0];
-        for(int i = 0; i < a.length; i++) {
-            if(min.compareTo(a[i]) > 0) min = a[i];
-            if(min.compareTo(a[i]) < 0) max = a[i];
+        for (String s : a) {
+            if (min.compareTo(s) > 0) min = s;
+            if (min.compareTo(s) < 0) max = s;
         }
         return new Pair<> (min, max);
     }
